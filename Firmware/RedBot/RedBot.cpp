1c4fe67c6d4f145e0982bb84e09f65129c5785ac
Zack Lalanne <zack.lalanne@gmail.com>
2014-06-14 11:20:32 -0500

    Working motors in Energia

---
+++ Firmware/RedBot/RedBot.cpp
@@ -0,0 +1,42 @@
+/****************************************************************
+Main CPP for RedBot. This file handles the pin change interrupts
+and how we multiplex between the different potential causes of
+a pin change interrupt.
+
+This code is beerware; if you use it, please buy me (or any other
+SparkFun employee) a cold beverage next time you run into one of
+us at the local.
+
+21 Jan 2014- Mike Hord, SparkFun Electronics
+
+Code developed in Arduino 1.0.5, on an SparkFun Redbot v12.
+****************************************************************/
+
+#include "RedBot.h"
+//#include <avr/interrupt.h>
+#include <Energia.h>
+
+// We need to track what the prior state of our pins for various PCINTS was;
+//  this varies by interrupt. These values are initialized to the "all high"
+//  state; we don't want any low-to-high transitions at beginning of code
+//  execution to be caught.
+volatile byte lastPC0PinState = 0x0E;  // For pins 9, 10, 11, PB1-3
+volatile byte lastPC1PinState = 0x3F;  // For pins A0-A5/14-19, PC0-5
+volatile byte lastPC2PinState = 0x08;  // For pin 3, PD3
+
+// We need some way to exclude short transients on the encoder inputs; we'll do
+//  that by capturing the most recent rise time with millis() and ignoring
+//  falling edges that happen within 20ms of a rise.
+volatile unsigned long lastRRise = 0;
+volatile unsigned long lastLRise = 0;
+#define  ENC_HIGH_DELAY   10
+
+byte PBMask = 0;
+byte PCMask = 0;
+byte PDMask = 0;
+
+volatile byte pinFunction[10];     // Store the currently assigned fucntion
+                                       //  of the PCINT associated with each pin
+                                       //  in this array. Array indices are of
+                                       //  the type "PCINT_pinname".
+                                       
---
+++ Firmware/RedBot/RedBot.h
@@ -0,0 +1,100 @@
+/****************************************************************
+Core header file for all the various RedBot functions.
+
+There is additional license info below regarding the use of the
+SoftwareSerial library from Arduino 1.0.5; I had good and sound
+reasons for creating a derivative class rather than asking users
+to simply use the existing library, which are documented below.
+
+This code is beerware; if you use it, please buy me (or any other
+SparkFun employee) a cold beverage next time you run into one of
+us at the local.
+
+21 Jan 2014- Mike Hord, SparkFun Electronics
+
+Code developed in Arduino 1.0.5, on an SparkFun Redbot v12.
+****************************************************************/
+
+#ifndef RedBot_h
+#define RedBot_h
+
+#include <Energia.h>
+
+// Pin aliases for the motor controller.
+#define    L_CTRL_1   34
+#define    L_CTRL_2   33
+#define    PWM_L      40
+
+#define    R_CTRL_1   32
+#define    R_CTRL_2   31
+#define    PWM_R      39
+
+// PCINT functionality aliases. Each PCINT has a value set up when the
+//  class member gets created, and the PCINT service routine will handle
+//  the choosing the appropriate response to the interrupt.
+
+#define NOT_IN_USE    0
+#define WHISKER       1
+#define LENCODER      2
+#define RENCODER      3
+#define SW_SERIAL     4
+
+#define PCINT_A0      0
+#define PCINT_A1      1
+#define PCINT_A2      2
+#define PCINT_A3      3
+#define PCINT_A4      4
+#define PCINT_A5      5
+#define PCINT_3       6
+#define PCINT_9       7
+#define PCINT_10      8
+#define PCINT_11      9
+
+enum WHEEL {LEFT, RIGHT, BOTH}; // Variable for which wheel you're interested in
+                  //  when you do things in the encoder class.
+
+// These three functions need to work from within multiple classes, so we keep
+//  them separate and add them as friend functions where appropriate.
+void setPinChangeInterrupt(int pin, byte role); // The "role" of each pin is
+                  //  stored in an array which is accessed in the interrupt
+                  //  handler to determine what should be done on a falling edge
+                  //  PC interrupt.
+void pinFunctionHandler(byte pinIndex); // This is the function which actually
+                  //  handles the legwork after the interrupt has identified
+                  //  which pin caught the interrupt.
+void brake(void); // Globally accessible motor brake. I couldn't figure out how
+                  //  to set a function pointer to the RedBotMotor class
+                  //  function, and it's a small function, so I just made a
+                  //  global in the library.
+void PC0Handler(byte PBTemp);
+void PC1Handler(byte PCTemp);
+void PC2Handler(byte PDTemp);
+
+
+// This class handles motor functionality. I expect one instance of this at the
+//  start of a piece of RedBot code.
+class RedBotMotor
+{
+  public:
+    RedBotMotor();          // Constructor. Mainly sets up pins.
+    void drive(int speed);  // Drive in direction given by sign, at speed given
+                            //  by magnitude of the parameter.
+    void pivot(int speed);  // Pivot more or less in place. Turns motors in
+                            //  opposite directions. Positive values correspond
+                            //  to anti-clockwise rotation.
+    void rightDrive(int speed); // Drive just the left motor, as drive().
+    void leftDrive(int speed);  // Drive just the right motor, as drive().
+    void stop();            // Stop motors, but allow them to coast to a halt.
+    void brake();           // Quick-stop the motors, shorting the leads.
+    void rightStop();       // Stop right motor, as with stop().
+    void leftStop();        // Stop left motor, as with stop().
+    void leftBrake();       // Quick-stop left motor, as with brake().
+    void rightBrake();      // Quick-stop right motor, as with brake().
+  private:
+    void leftFwd(byte speed); // These functions are pretty self-explanatory,
+    void leftRev(byte speed); //  and are called by the above functions once
+    void rightFwd(byte speed);//  sign has been used to determine direction.
+    void rightRev(byte speed);
+};
+
+#endif
